9c8cbbb0c8c6fa209c90632a27ae19a1f8c5659b
Ignacio Rodrigo <irodrigo17@gmail.com>
2016-01-08 23:13:38 -0300

    Created model, updated admin section

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from tickets.models import Ticket
 
 # Register your models here.
+class TicketAdmin(admin.ModelAdmin):
+	list_display = ('title','author','created_date', 'status')
+	list_filter = ('created_date','title', 'author', 'assignee')
+	
+admin.site.register(Ticket, TicketAdmin)
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Ticket(models.Model):
+    STATUSES = (
+        (0, 'open'),
+        (1, 'closed'),
+    )
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=200)
+    status = models.IntegerField(max_length=1, choices=STATUSES)
+    author = models.ForeignKey(User, related_name='author')
+    assignee = models.ForeignKey(User, related_name='assignee')
+    created_date = models.DateTimeField(auto_now_add=True)
